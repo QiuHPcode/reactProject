b8dfafbd98f2847dcb0409b4bb49b504b76a0578
Qiu <12813092+qhpcode@user.noreply.gitee.com>
2025-03-30 17:24:25 +0800

    feature: umi简单应用

---
+++ my-single-umi/.umirc.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from "umi";
+
+export default defineConfig({
+  routes: [
+    { path: "/", component: "index" },
+    { path: "/docs", component: "docs" },
+    { path: "/app", component: "./App"}
+  ],
+  npmClient: 'pnpm',
+  alias: {
+
+  },
+  // proxy: {
+  //   '/api': {
+  //     'target': 'http://jsonplaceholder.typicode.com/',
+  //     'changeOrigin': true,
+  //     'pathRewrite': { '^/api' : '' },
+  //   }
+  // }
+});
---
+++ my-single-umi/src/pages/App/index.tsx
@@ -0,0 +1,10 @@
+const DocsPage = () => {
+    return (
+      <div>
+        <p>This is umi app.</p>
+      </div>
+    );
+  };
+  
+  export default DocsPage;
+  
